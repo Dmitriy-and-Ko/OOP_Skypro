e40b35c30592e8b12d14527e2ebdb81e19210c9b
Dmitriy Kovshov <dmitriykovshov32@gmail.com>
2025-01-11 16:53:01 +0300

    task_2 fixed

--- src/category.py
+++ src/category.py
@@ -1,5 +1,14 @@
 
-class Catrgory:
+class Category:
     name: str
     description: str
     products: list
+    category_quantity = 0
+    products_quantity = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+        Category.category_quantity += 1
+        Category.products_quantity += len(products)
